9b3d0866d39777562dda8dc5a0a14e2157631334
shizunari <mare.fe.2006+biz@gmail.com>
2026-07-30 15:50:31 +0900

    [Change]日/英語の切り替えリンクを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
-    locale = I18n.default_locale unless I18n.available_locales.include?(locale.to_sym)
+    locale = I18n.default_locale unless I18n.available_locales.include?(locale.to_s.to_sym)
     I18n.with_locale(locale, &action)
   end
 
