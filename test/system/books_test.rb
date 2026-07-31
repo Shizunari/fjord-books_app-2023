1d99254d7167bb1d80f8dff5f4837efe54255b49
shizunari <mare.fe.2006+biz@gmail.com>
2026-07-31 14:47:11 +0900

    [Change] テストモジュールの修正

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -9,37 +9,45 @@ class BooksTest < ApplicationSystemTestCase
 
   test 'visiting the index' do
     visit books_url
-    assert_selector 'h1', text: 'Books'
+    assert_selector 'h1', text: I18n.t('.books.index.html_title')
   end
 
   test 'should create book' do
     visit books_url
-    click_on 'New book'
+    click_on I18n.t('.books.index.new_book')
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in I18n.t('.activerecord.attributes.book.memo'), with: @book.memo
+    fill_in I18n.t('.activerecord.attributes.book.title'), with: @book.title
+    click_on I18n.t('.helpers.submit.create', model: I18n.t('.activerecord.models.book'))
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    assert_text I18n.t('.flash.books.create.notice')
+    click_on I18n.t('.books.new.link_index')
   end
 
   test 'should update Book' do
     visit book_url(@book)
-    click_on 'Edit this book', match: :first
+    click_on I18n.t('.books.show.link_edit'), match: :first
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+    fill_in I18n.t('.activerecord.attributes.book.memo'), with: @book.memo
+    fill_in I18n.t('.activerecord.attributes.book.title'), with: @book.title
+    click_on I18n.t('.helpers.submit.update', model: I18n.t('.activerecord.models.book'))
 
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    assert_text I18n.t('.flash.books.update.notice')
+    click_on I18n.t('.books.new.link_index')
   end
 
   test 'should destroy Book' do
     visit book_url(@book)
-    click_on 'Destroy this book', match: :first
+    click_on I18n.t('.books.show.delete_book'), match: :first
 
-    assert_text 'Book was successfully destroyed'
+    assert_text I18n.t('.flash.books.destroy.notice')
+  end
+
+  test 'should switch language' do
+    visit books_url
+    click_on I18n.t('.links.language_switch')
+
+    test_locale = I18n.locale == :ja ? :en : :ja
+    assert_selector 'h1', text: I18n.t('.books.index.html_title', locale: test_locale)
   end
 end
