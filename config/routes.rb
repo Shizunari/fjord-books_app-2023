8743db51dea712a3314829a1b5363c1bd984df09
shizunari <mare.fe.2006+biz@gmail.com>
2026-09-04 07:31:33 +0900

    [New] Devise課題を一通り完了（不要なファイル込み）
    - rubocopも一部「c」が残っているが、不要なファイルに該当するものもある。
    - letter_opener_web gemを使用している。
    - ViewとControllerの削除前に念のためCommitする。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   resources :books
+  resources :users, only: %i[index show]
+  root "books#index"
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
